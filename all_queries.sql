261fb31e9f9fffb9e3a9b00ecfcef8abebefd079
Saif <droconos99@gmail.com>
2023-10-20 22:48:32 +0600

    created tables containting admin requests

--- all_queries.sql
+++ all_queries.sql
@@ -10,3 +10,36 @@ ALTER table admin ADD column total_lent_amount INT;
 
 insert into admin(name,email,password,total_bank_deposits,total_lent_amount) values('saif','saif981@gmail.com','saif123',0,0);
 select * from admin;
+
+CREATE TABLE transfer(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    sender INT NOT NULL,
+    receiver INT NOT NULL,
+    amount INT
+);
+
+CREATE TABLE loan(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    account INT NOT NULL,
+    amount INT,
+    admin_id INT NOT NULL
+);
+
+CREATE TABLE deposit(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    account INT NOT NULL,
+    amount INT,
+    admin_id INT NOT NULL
+);
+
+CREATE TABLE withdraw(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    account INT NOT NULL,
+    amount INT,
+    admin_id INT NOT NULL
+);
+
+show tables;
+
+
+
