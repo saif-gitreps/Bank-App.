f56c0af5519de48e404db5430d0221cec383539e
Saif <droconos99@gmail.com>
2023-10-21 11:53:56 +0600

    fixed server side bug (issue:did not install ejs package)

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
+const fs = require("fs");
 const path = require("path");
 const express = require("express");
-const exp = require("constants");
 const app = express();
 
-app.set("view engines", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
-app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 app.get("/", (request, response) => {
    response.render("index");
 });
 
-app.use((request, response) => {
-   response.status(500).send("<h1>404 web page not found!</h1>");
+app.get("/home", (request, response) => {
+   response.render("index");
 });
-app.use((error, request, response, next) => {
-   response.send("<h1>Sorry some server issue!</h1>");
-});
+
+// app.use((request, response) => {
+//    response.status(500).send("<h1>404 web page not found!</h1>");
+// });
+// app.use((error, request, response, next) => {
+//    response.send("<h1>Sorry some server issue!</h1>");
+// });
 
 app.listen(3000, () => {
    console.log("server initiated.");
