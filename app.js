3379a23c66b9a166841063b8e5cd45895b28bec6
Saif <droconos99@gmail.com>
2023-10-21 22:43:31 +0600

    added routes for transfering

--- app.js
+++ app.js
@@ -17,8 +17,7 @@ app.post("/login", async (request, response) => {
    const loginData = request.body;
 
    if (loginData.userselection == "admin") {
-      const query = `select * from admin where email = ?`;
-      const data = await db.query(query, [loginData.useremail]);
+      const data = await db.query("select * from admin where email = ?", [loginData.useremail]);
       const adminData = data[0];
       if (adminData.length == 0) {
          response.render("index", { message: "No such Administrator exist." });
@@ -32,7 +31,7 @@ app.post("/login", async (request, response) => {
       const customerData = data[0];
       if (customerData.length == 0) {
          response.render("index", {
-            message: "No such email exists, create an account to login.",
+            message: "No such email exists, Create an account to login.",
          });
       } else if (customerData[0].password != loginData.userpassword) {
          response.render("index", { message: "Incorrect password, please retry again." });
@@ -51,17 +50,32 @@ app.get("/create-account", (request, response) => {
 });
 
 app.get("/admin-home/:id", async (request, response) => {
-   const query = `select * from admin where id = ?`;
-   const data = await db.query(query, [request.params.id]);
+   const data = await db.query("select * from admin where id = ?", [request.params.id]);
    const adminData = data[0];
    response.render("admin-home", { adminData: adminData[0] });
 });
 
 app.get("/client-page/:id", async (request, response) => {
-   const query = `select * from customer where account_no = ?`;
    const data = await db.query("select * from customer where account_no = ?", [request.params.id]);
    const customerData = data[0];
-   response.render("client-page", { customerData: customerData[0] });
+   response.render("client-page", { customerData: customerData[0], m1: "", m2: "", m3: "", m4: "" });
+});
+
+app.post("/client-transfer/:id", async (request, response) => {
+   const requestData = request.body;
+   const senderData = await db.query("select * from customer where account_no = ?", [request.params.id]);
+   const receiverData = await db.query("select * from customer where account_no = ?", [
+      requestData.receivingaccount,
+   ]);
+   if (receiverData[0].length == 0) {
+      return response.render("client-page", {
+         customerData: senderData[0][0],
+         m1: "No such account exist",
+         m2: "",
+         m3: "",
+         m4: "",
+      });
+   }
 });
 
 // app.use((request, response) => {
