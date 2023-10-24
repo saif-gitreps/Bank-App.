23ca621dc8a87ce4cd6aa3164c7b291108828ff4
Saif <droconos99@gmail.com>
2023-10-24 20:43:39 +0600

    added routes and sql queries for adding new customers through create-account form

--- app.js
+++ app.js
@@ -51,7 +51,33 @@ app.get("/client", (request, response) => {
 });
 
 app.get("/create-account", (request, response) => {
-   response.render("register-page");
+   response.render("register-page", { m1: "" });
+});
+
+app.post("/create-account", async (request, response) => {
+   const formData = request.body;
+   console.log(formData);
+   const currentCustomers = await db.query("select * from customer where email = ?", [
+      formData.useremail,
+   ]);
+   const admin = await db.query("select * from admin where email = ?", [
+      formData.useremail,
+   ]);
+   if (currentCustomers[0].length != 0 || admin[0].length != 0) {
+      return response.render("register-page", {
+         m1: "email already exist, choose a different one.",
+      });
+   }
+   if (formData.userpassword1 != formData.userpassword2) {
+      return response.render("register-page", { m1: "passwords do not match" });
+   }
+   await db.query(
+      "insert into customer(name,email,password,balance,loan) value(?,?,?,?,?)",
+      [formData.username, formData.useremail, formData.userpassword1, 0, 0]
+   );
+   response.send(
+      `<h3 style="text-align:center;">Account created, click <a href="/">here</a> to login.</h3>`
+   );
 });
 
 app.get("/admin-home/:id", async (request, response) => {
